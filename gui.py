1557863503db79d9e380f0a95e948895aea74cac
Yougraj <yougrajbora.developer@gmail.com>
2023-03-24 17:21:25 +0530

    Adding the complete, edit, and exit funtionality

--- gui.py
+++ gui.py
@@ -8,12 +8,14 @@ add_button = sg.Button("Add")
 list_box = sg.Listbox(values=function.get_todos(), key='todos', enable_events=True,
                       size=[45, 10])
 edit_button = sg.Button("Edit")
-
-
+complete_button = sg.Button("Complete")
+exit_button = sg.Button("Exit")
 
 window = sg.Window("My To-Do App", 
-                   layout=[[label], [input_box, add_button],
-                           [list_box, edit_button]], 
+                   layout=[[label],
+                            [input_box, add_button],
+                            [list_box, edit_button, complete_button],
+                            [exit_button]], 
                    font=("Helvetica", 20))
 
 
@@ -28,7 +30,7 @@ while True:
             todos.append(new_todos)
             function.write_todos(todos)
             window['todos'].update(values=todos)
-            
+
         case 'Edit':
             todo_to_edit = values['todos'][0]
             new_todo = values['todo']
@@ -38,6 +40,16 @@ while True:
             todos[index] = new_todo
             function.write_todos(todos)
             window['todos'].update(values=todos)
+        case 'Complete':
+            todo_to_complete = values['todos'][0]
+            todos = function.get_todos()
+            todos.remove(todo_to_complete)
+            function.write_todos(todos)
+            window['todos'].update(values=todos)
+            window['todo'].update(value='')
+        
+        case 'Exit':
+            break
 
         case 'todos':
             window['todo'].update(value=values['todos'])
