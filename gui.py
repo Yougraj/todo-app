a87e9061ef728a14b515cc553727a8330dd04560
Yougraj <yougrajbora.developer@gmail.com>
2023-03-25 14:49:35 +0530

    removing images

--- gui.py
+++ gui.py
@@ -12,13 +12,11 @@ sg.theme("Black")
 clock = sg.Text('', key="Clock")
 label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="Enter todo", key='todo') 
-add_button = sg.Button(size=2, image_source="add.png", mouseover_colors="LightBlue2",
-                       tooltip="todo add", key="Add")
+add_button = sg.Button('Add')
 list_box = sg.Listbox(values=function.get_todos(), key='todos', enable_events=True,
                       size=[45, 10])
 edit_button = sg.Button("Edit")
-complete_button = sg.Button(key="Complete", size=2, image_source="complete.png", mouseover_colors="LightBlue2",
-                            tooltip="Complete the todo")
+complete_button = sg.Button('Complete')
 exit_button = sg.Button("Exit")
 
 window = sg.Window("My To-Do App", 
